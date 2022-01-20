c0de2c82c53658f747d3f40f6232638058433f7c
Tzurrr <zur001@gmail.com>
2022-01-20 09:10:07 +0200

    added the main class of the program

---
+++ src/main/java/DB/DBMAIN.java
@@ -0,0 +1,11 @@
+package DB;
+
+import java.io.IOException;
+
+public class DBMAIN {
+    public static void main(String[] args) throws IOException {
+        CCSVDB a = new CCSVDB("ggg");
+        //a.Initializer();
+        a.addARow();
+    }
+}
