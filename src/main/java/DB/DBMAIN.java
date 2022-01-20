bd0aa52d3f8eb2a17094a47f12a3ca119d6948b5
Tzurrr <zur001@gmail.com>
2022-01-20 21:09:06 +0200

    checked some more functions

--- src/main/java/DB/DBMAIN.java
+++ src/main/java/DB/DBMAIN.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 public class DBMAIN {
     public static void main(String[] args) throws IOException {
         CCSVDB a = new CCSVDB("ggg");
+        CCSVDB new1 = new CCSVDB("aaa");
+        new1.Initializer();
+        new1.addARow();
         //a.Initializer();
         //a.addARow();
         //a.addARow();
         //a.deleteTable();
         //a.deleteLine(1);
         //a.updateARow(1);
-        a.printTable();
+        //a.printTable();
     }
 }
